8d3dc8b6d96091ecd0223e5bddf1a506bfb656fe
yosef goren <yosefisraelgoren@gmail.com>
2024-12-05 21:54:30 +0200

    sample updated

--- example.cpp
+++ example.cpp
@@ -1,14 +1,16 @@
 #include <stdio.h>
-#include <omp.h>
+#include <unistd.h>
+
+#define MICROSEC_IN_SEC (1000000)
 
 int main(){
 	//@#$init
 	for(int i = 0; i < 4; ++i) {
-		// printf("things\n");
+		usleep(MICROSEC_IN_SEC*0.2);
 	}
-	for(int j = 0; j < 15; ++j) {
-		for(int x = 0; x < 15; ++x) {
-			// printf("stuff\b");
+	for(int j = 0; j < 10; ++j) {
+		for(int x = 0; x < 5; ++x) {
+			usleep(MICROSEC_IN_SEC*0.02);
 		}
 	}
 	//@#$finish
