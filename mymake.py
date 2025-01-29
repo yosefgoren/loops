7f0544c70b75282de22467b35c2302d5f5ee4526
yosef goren <yosefisraelgoren@gmail.com>
2025-01-29 21:04:37 +0200

    version working with file modifications and per-thread count sampling

--- mymake.py
+++ mymake.py
@@ -18,52 +18,6 @@ def pymodule_script_prefix(script_path: str) -> str:
     no_suffix_path = script_path[:-3]
     return f"python3 -m {no_suffix_path.replace('/', '.')}"
 
-def nas_collection_rules(bench_name: str, num_threads: int) -> tuple[list[Rule], CreatedFileNode]:
-    """
-    Returns a generated samples node and a list of rules required for creating it.
-    """
-
-    do_parallel: bool = num_threads > 1
-    rules: list[Rule] = []
-
-    basename = f"{bench_name}_{num_threads}"
-    
-    # src_file is never a dependency since it is modified (and will cause incorrect reconstruction)
-    src_file = StaticFileNode(f"{BASEDIR}/{bench_name.upper()}/{bench_name}.cpp")
-
-    src_file_copy = CreatedFileNode(f"{BASEDIR}/{bench_name.upper()}/orig_{bench_name}.cpp")
-    rules.append(ShellRule(src_file_copy, [], f"cp {src_file.path} {src_file_copy.path}"))
-
-    scopes = CreatedFileNode(f"./samples/{bench_name}.scopes.json")
-    rules.append(ShellRule(scopes, [scopes_script],
-        f"{pymodule_script_prefix(scopes_script.path)} --input {src_file.path} -o {scopes.path}"
-    ))
-
-    targets = CreatedFileNode(f"./samples/{bench_name}.targets.json")
-    rules.append(ShellRule(targets, [scopes, targets_script],
-        f"{pymodule_script_prefix(targets_script.path)} --input {scopes.path} -o {targets.path}"
-    ))
-
-    times_log = CreatedFileNode(f"./samples/{basename}.times")
-
-    mod = FileModificationNode(src_file, f"{num_threads}")
-    rules.append(ShellFileModifyRule(mod, [src_file, targets, modify_script],
-        f"{pymodule_script_prefix(modify_script.path)} --read_file {src_file.path} --write_file {src_file.path} --logs_filename {times_log.path} --tgt_file {targets.path} --parallel {'True' if do_parallel else 'False'}"
-    ))
-
-    timed_executable = CreatedFileNode(f"{BINDIR}/{bench_name}.S")
-    rules.append(ShellRule(timed_executable, [mod, copied_timer],
-        f"make -C {BASEDIR} {bench_name}"
-    ))
-
-    rules.append(ShellRule(times_log, [timed_executable], f"OMP_NUM_THREADS={num_threads} {timed_executable.path}"))
-
-    samples = CreatedFileNode(f"./samples/{bench_name}.samples.json")
-    rules.append(ShellRule(samples, [times_log, targets, collect_script, src_file_copy],
-        f"{pymodule_script_prefix(collect_script.path)} -o {samples.path} --runtimes_file {times_log.path} --source_file {src_file_copy.path} --loops_file {targets.path}"
-    ))
-    
-    return rules, samples
 
 
 all_rules: list[Rule] = []
@@ -78,20 +32,54 @@ all_rules.append(ShellRule(copied_timer, [timer_cpp_lib],
 
 
 # Add rules for each benchmark:
-for thread_cnt in [1, 16]:
-    for bench in [
-        "bt",
-        "cg",
-        "ep",
-        "ft",
-        "is",
-        "lu",
-        "mg",
-        "sp",
-    ]:
-        new_rules, node = nas_collection_rules(bench, thread_cnt)
-        sample_nodes.append(node)
-        all_rules += new_rules
+for bench_name in [
+    "bt",
+    "cg",
+    "ep",
+    "ft",
+    "is",
+    "lu",
+    "mg",
+    "sp",
+]:
+    # src_file is never a dependency since it is modified (and will cause incorrect reconstruction)
+    src_file = StaticFileNode(f"{BASEDIR}/{bench_name.upper()}/{bench_name}.cpp")
+
+    src_file_copy = CreatedFileNode(f"{BASEDIR}/{bench_name.upper()}/orig_{bench_name}.cpp")
+    all_rules.append(ShellRule(src_file_copy, [], f"cp {src_file.path} {src_file_copy.path}"))
+
+    scopes = CreatedFileNode(f"./samples/{bench_name}.scopes.json")
+    all_rules.append(ShellRule(scopes, [scopes_script],
+        f"{pymodule_script_prefix(scopes_script.path)} --input {src_file.path} -o {scopes.path}"
+    ))
+
+    targets = CreatedFileNode(f"./samples/{bench_name}.targets.json")
+    all_rules.append(ShellRule(targets, [scopes, targets_script],
+        f"{pymodule_script_prefix(targets_script.path)} --input {scopes.path} -o {targets.path}"
+    ))
+    for num_threads in [1, 16]:
+        do_parallel: bool = num_threads > 1
+
+        basename = f"{bench_name}_{num_threads}"
+        times_log = CreatedFileNode(f"./samples/{basename}.times")
+
+        mod = FileModificationNode(src_file, f"{num_threads}")
+        all_rules.append(ShellFileModifyRule(mod, [src_file, targets, modify_script],
+            f"{pymodule_script_prefix(modify_script.path)} --read_file {src_file.path} --write_file {src_file.path} --logs_filename {times_log.path} --tgt_file {targets.path} --parallel {'True' if do_parallel else 'False'}"
+        ))
+
+        executable_path: str = f"{BINDIR}/{bench_name}.S" # For example './NPB-CPP/NPB-OMP/bin/bt.S'
+        all_rules.append(ShellRule(times_log, [mod, copied_timer],
+            f"make -C {BASEDIR} {bench_name} && OMP_NUM_THREADS={num_threads} {executable_path}" # Compile and run the benchmark
+        ))
+
+        samples = CreatedFileNode(f"./samples/{basename}.samples.json")
+        all_rules.append(ShellRule(samples, [times_log, targets, collect_script, src_file_copy],
+            f"{pymodule_script_prefix(collect_script.path)} -o {samples.path} --runtimes_file {times_log.path} --source_file {src_file_copy.path} --loops_file {targets.path}"
+        ))
+        
+
+        sample_nodes.append(samples)
 
 dataset_name = "ompcpp"
 dataset_files = CreatedFileNode(f"{dataset_name}.train.jsonl") # TODO: add MAKE-API feature for target with multiple files, and use it here to make ompcpp.validate.jsonl another declared target.
